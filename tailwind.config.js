d16018bc328226531e38412cc18df2c8a340671d
Osher <osherasfa@gmail.com>
2023-11-12 20:19:38 +0200

    Initialize project's files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        'spaceGrotesk': ['Space Grotesk', 'sans-serif'],
+      },
+      colors: {
+        'palette': {
+          'blue': '#377DFF',
+          'green': '#38CB89',
+          'yellow': '#FFAB00',
+          'red': '#FF5630',
+          'brandColor': '#000000',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
